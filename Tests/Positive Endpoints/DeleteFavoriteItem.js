ae0b2604680f6029654562ad23d98f3ec2f8c543
nejlaBelagosi <122165597+nejlaBelagosi@users.noreply.github.com>
2025-09-15 10:37:19 +0200

    Create DeleteFavoriteItem.js

---
+++ Tests/Positive Endpoints/DeleteFavoriteItem.js
@@ -0,0 +1,20 @@
+const jsonData = pm.response.json();
+
+pm.test("Status code is 200", function () {
+    pm.response.to.have.status(200);
+});
+
+//Response time is below 1000ms
+pm.test("Response time is below 1000ms", function(){
+    pm.expect(pm.response.responseTime).to.be.below(1000);
+})
+
+//check API Url endpoint is valid
+pm.test("API URL endpoint is valid", function(){
+    pm.expect(pm.request.url.toString()).to.eql(`${pm.environment.get('baseUrl')}/api/Favorite/DeleteFavorite/${pm.environment.get('favoriteItemId')}`);
+})
+
+//check header content type
+pm.test("Content-Type is application/json", function(){
+    pm.expect(pm.response.headers.get("Content-Type")).to.include("application/json");
+})
