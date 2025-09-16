82e7092ce0c4512ff12933af56e4bcb936ca9b28
nejlaBelagosi <122165597+nejlaBelagosi@users.noreply.github.com>
2025-09-16 16:58:53 +0200

    Create DeleteReview.js

---
+++ Tests/Positive Endpoints/DeleteReview.js
@@ -0,0 +1,18 @@
+pm.test("Status code is 200", function () {
+    pm.response.to.have.status(200);
+});
+
+//Response time is below 1000ms
+pm.test("Response time is below 1000ms", function(){
+    pm.expect(pm.response.responseTime).to.be.below(1000);
+})
+
+//check API Url endpoint is valid
+pm.test("API URL endpoint is valid", function(){
+    pm.expect(pm.request.url.toString()).to.eql(`${pm.environment.get('baseUrl')}/api/Review/DeleteReview/${pm.environment.get('reviewId')}`);
+})
+
+//check header content type
+pm.test("Content-Type is application/json", function(){
+    pm.expect(pm.response.headers.get("Content-Type")).to.include("application/json");
+});
