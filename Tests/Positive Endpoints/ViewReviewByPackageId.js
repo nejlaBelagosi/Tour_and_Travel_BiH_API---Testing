5d3746c185fec2cc2435d342a05ffd3242544485
nejlaBelagosi <122165597+nejlaBelagosi@users.noreply.github.com>
2025-09-16 17:16:02 +0200

    Create ViewReviewByPackageId.js

---
+++ Tests/Positive Endpoints/ViewReviewByPackageId.js
@@ -0,0 +1,28 @@
+pm.test("Status code is 200", function () {
+    pm.response.to.be.oneOf([200,204]);
+});
+
+//is JSON Valid
+pm.test("Response is valid JSON", function(){
+    pm.expect(pm.response.json());
+});
+
+//check response headers
+pm.test("Response headers are valid", function(){
+    pm.expect(pm.response.headers.get("Content-Type")).to.include("application/json");
+});
+
+//check request url
+pm.test("Request Url is valid", function(){
+    pm.expect(pm.request.url.toString()).to.eql(`${pm.environment.get('baseUrl')}/api/Review/GetReviewByPackageId/GetReviewByPackageId/${pm.environment.get('tourPackageId')}`);
+});
+
+//check API response time is within acceptable limits
+pm.test("API response time is within acceptable limits", function(){
+    pm.expect(pm.response.responseTime).to.be.below(500);
+});
+
+//check API Payload size is within acceptable limits
+pm.test("API payload size is wiyhin acceptable limits", function(){
+     pm.expect(pm.request.body.raw.length).to.be.below(1024);
+})
